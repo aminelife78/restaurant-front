64b398caa81743e6119543328909adca5b817b01
med amine <aminelife86@gmail.com>
2023-01-02 16:59:09 +0100

    save local

--- src/Api/baseUrl.js
+++ src/Api/baseUrl.js
@@ -1,4 +1,4 @@
-// const baseUrl = "http://localhost:5000";
-const baseUrlProd = "https://api-restaurant-production.up.railway.app";
+const baseUrlProd = "http://localhost:5000";
+// const baseUrlProd = "https://api-restaurant-production.up.railway.app";
 
 export default baseUrlProd;
